0d1856bcc5f67441aee62101edf3581451fa8d58
Edward Nimocks <enimocks@gmail.com>
2018-07-06 14:12:33 -0400

    more setup

--- src/components/CharCard/CharCard.js
+++ src/components/CharCard/CharCard.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import 'CharCard.css';
 
-
-
+//pass each image into a card to get all 12
+const CharCard = props => (
+  <div className="card" onClick={props.imageClick}>
+    <div className="img-container">
+      <img alt={props.image.replace(".jpg", "")} src={require("../../images/" + props.image)} />
+    </div>
+  </div>
+);
 
 export default CharCard;
