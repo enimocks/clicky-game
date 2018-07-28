e702e657dcf73a9b47ae2ea5ebf54cbfbb0871e9
Edward Nimocks <enimocks@gmail.com>
2018-07-27 20:47:18 -0400

    add README

--- src/components/CharCard/CharCard.js
+++ src/components/CharCard/CharCard.js
@@ -3,11 +3,13 @@ import './CharCard.css';
 
 // pass each image into a card to get all 12
 const CharCard = props => (
+  <div className="col-xs-12">
     <div className="card" onClick={() => props.handleClick(props.id)}>
       <div className="img-container">
         <img alt={props.image.replace(".jpg", "")} src={require("../../img/" + props.image)} />
       </div>
     </div>
+  </div>
 );
 
 export default CharCard;
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Title.css";
 
 const Title = () => (
-  <header>
+  <header className="mx-auto">
     <h1>Try to click each tile once, and only once!</h1>
   </header>
 )
